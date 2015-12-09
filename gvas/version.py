24143d74fe7e1059d24efda106c688b7b443f9c9
Benjamin Bengfort <benjamin@bengfort.com>
2015-12-09 12:50:49 -0500

    version bump

--- gvas/version.py
+++ gvas/version.py
@@ -19,7 +19,7 @@ Helper module for GVAS version information
 
 __version_info__ = {
     'major': 0,
-    'minor': 1,
+    'minor': 3,
     'micro': 0,
     'releaselevel': 'final',
     'serial': 0,
